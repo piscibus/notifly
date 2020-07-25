d9a88e9f78bb23e7fc94b19c980d6cba38290c2e
imdhemy <imdhemy@gmail.com>
2020-07-25 20:14:14 +0200

    Replace in memory with db file

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -32,7 +32,7 @@ class TestCase extends Orchestra
         $app['config']->set('database.default', 'sqlite');
         $app['config']->set('database.connections.sqlite', [
             'driver' => 'sqlite',
-            'database' => ':memory:',
+            'database' => __DIR__ . '/sqlite.db',
             'prefix' => '',
         ]);
 
