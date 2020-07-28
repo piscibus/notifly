c493a60f6253f14a8fecdf9d6e5aa6e0cabbdd62
imdhemy <imdhemy@gmail.com>
2020-07-28 17:14:51 +0200

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,7 @@ use CreateExampleObjectTable;
 use CreateExampleTargetTable;
 use CreateNotiflyNotificationActorTable;
 use CreateNotiflyNotificationTable;
-use CreateNotiflyReadNotificationsTable;
+use CreateNotiflyReadNotificationTable;
 use CreateUserTable;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Piscibus\Notifly\NotiflyServiceProvider;
@@ -41,7 +41,7 @@ class TestCase extends Orchestra
         include_once __DIR__ . '/../database/migrations/create_notifly_notification_actor_table.php.stub';
 
         (new CreateNotiflyNotificationTable())->up();
-        (new CreateNotiflyReadNotificationsTable())->up();
+        (new CreateNotiflyReadNotificationTable())->up();
         (new CreateNotiflyNotificationActorTable())->up();
 
         include_once __DIR__ . '/database/migrations/CreateUserTable.php';
